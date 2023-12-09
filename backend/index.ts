8a3679b217764c96a91a22e1df1f11836bde573e
CLL <140147262+CLcosep@users.noreply.github.com>
2023-12-09 19:43:19 +0800

    added

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,13 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import cors from 'cors'
 //For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
-
+app.use(cors());
 app.use('/',router);
 
 app.listen(port, () => {
