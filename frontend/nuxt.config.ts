aafc23c8e77de6a9a00a58d9b4538d758e501795
CLL <140147262+CLcosep@users.noreply.github.com>
2024-01-08 01:25:06 +0800

    ass

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {public: {API: process.env.API}}
+  // const API = runtimeConfig().public.API
 })
